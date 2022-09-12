338b6110cc9d4187f321d3c5eadcbc03e030fc5a
Seunggu Kim <reizel@naver.com>
2022-09-12 18:37:23 +0900

    pyWorld

--- beexpress/static/js/index.js
+++ beexpress/static/js/index.js
@@ -1,5 +1,5 @@
-async function getMessage() {
-    let message = await fetch('/api/message/get').then(response => {
+function getMessage() {
+    let message = fetch('/api/message/get').then(response => {
         return response.text();
     }).catch(error => {
         console.log('[Error]fetch.api.message.get:', error);
