7592e0076414f4e31f18b2228046b4b97adf6b85
Seunggu Kim <reizel@naver.com>
2023-03-24 10:29:20 +0900

    Add src/app.js to fereact

---
+++ fereact/src/app.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  let [message, setMessage] = useState('');
+
+  function getMessage() {
+    fetch('/api/message/get').then((response) => {
+      return response.text();
+    }).then((resBody) => {
+      setMessage(resBody);
+    }).catch((error) => {
+      console.log('[Error]getMessage():', error);
+    });
+  }
+
+  useEffect(() => {
+    getMessage();
+    console.log('message:', message);
+  }, [message]);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
